e04ce90159ccbdc9e79a6c5cfa4bafd57b186393
gomes1876 <gomes1876@gmail.com>
2023-01-26 23:06:01 -0300

    create

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <tButton type="contained" size="xl">
+      <span>E. Large</span>
+    </tButton>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import tButton from 'test-lib-components';
+
+export default {
+  name: 'App',
+  components: {
+    tButton
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
